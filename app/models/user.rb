cbabef9414ee6feea8decf3bde39b214a51972d0
Liyana Roslie <liyanaroslie@Liyanas-MacBook-Air.local>
2018-08-03 14:13:46 +0800

    Add email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true, email: {strict_mode: true}
   validates :password, presence: true
 
   has_many :articles

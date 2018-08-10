f34a5072f69eb18c473c45085aa940bbb08dfcd4
Liyana Roslie <liyanaroslie@Liyanas-MacBook-Air-2.local>
2018-08-10 10:50:32 +0800

    Add redcarpet markdown feature

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
+  def markdown(content)
+    @markdown ||= Redcarpet::Markdown.new(Redcarpet::Render::HTML, {
+      autolink: true,
+      space_after_headers: true,
+      fenced_code_blocks: true,
+      filter_html: true,
+      hard_wrap: false,
+      strikethrough: true,
+      superscript: true,
+      autolink: true,
+      tables: true,
+      no_intra_emphasis: true,
+      underline: true,
+      highlight: true,
+      footnotes: true,
+      tables: true
+    })
+    @markdown.render(content).html_safe
+  end
 end
